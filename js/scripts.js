96ebc3f19c8de0c09ccb77593e4e416db2bafa2d
ngetichnicholas <ngetichnicholas903@gmail.com>
2021-04-28 09:14:42 +0300

    Update tasks changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-
+$(".tick").click(function() {
+    $(this).siblings().addClass("default-background-color");
+    $(this).class(".task").addClass("default-background-color");
+})
 });
