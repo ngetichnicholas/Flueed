07882cb1b75b8b3ee32331ecdd97118c8d9a9cfc
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-04-30 11:14:56 +0300

    Change directory structure for images.

--- js/scripts.js
+++ js/scripts.js
@@ -22,47 +22,47 @@ var date = new Date();
 let john = new Member(
 	"John Doe",
 	"johndoe@mail.com",
-	"./img/profile-pics/john.jpeg"
+	"../img/john.jpeg"
 );
 let jane = new Member(
 	"Jane Doe",
 	"janedoe@mail.com",
-	"./img/profile-pics/jane.jpeg"
+	"./img/jane.jpeg"
 );
 let nicholas = new Member(
 	"Nicholas Ngetich",
 	"johndoe@mail.com",
-	"../img/profile-pics/nicholas.jpeg"
+	"../img/nicholas.jpeg"
 );
 let peter = new Member(
 	"Peter Kennedy",
 	"johndoe@mail.com",
-	"../img/profile-pics/peter.jpeg"
+	"../img/peter.jpeg"
 );
 let odero = new Member(
 	"Odero Oluoch",
 	"odero@mail.com",
-	"../img/profile-pics/odero.jpeg"
+	"../img/odero.jpeg"
 );
 let festus = new Member(
 	"Festus Mutie",
 	"festus@mail.com",
-	"../img/profile-pics/festus.jpeg"
+	"../img/festus.jpeg"
 );
 let samora = new Member(
 	"Samora Yommie",
 	"samora@mail.com",
-	"../img/profile-pics/samora.jpeg"
+	"../img/samora.jpeg"
 );
 let floice = new Member(
 	"Floice Nyoita",
 	"floice@mail.com",
-	"/img/profile-pics/floice.jpeg"
+	"/img/floice.jpeg"
 );
 let aspin = new Member(
 	"Aspin Isoe",
 	"aspin@mail.com",
-	"../img/profile-pics/aspin.jpeg"
+	"../img/aspin.jpeg"
 );
 
 let marketing = new Team("Marketing", [floice, odero]);
